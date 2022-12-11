5587a8cb5eaa85ae897a94a620f94cf723358244
Asilhan Abdikulov <asilhanabdikulov@gmail.com>
2022-12-12 03:00:16 +0600

    project 5.1

---
+++ app/src/main/java/com/example/shop/CreateNodeActivity.kt
@@ -0,0 +1,28 @@
+package com.example.shop
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import android.widget.ImageView
+
+class CreateNodeActivity : AppCompatActivity(R.layout.activity_create_node) {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_node)
+
+
+
+      //  ImageView() image = findViewById(R.id.imageView4);
+    }
+
+
+   // private fun ImageView(): Any {
+
+   // }
+}
