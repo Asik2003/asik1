b5c6b0038677cbacf977281db829b1d62d368527
Asilhan Abdikulov <asilhanabdikulov@gmail.com>
2022-12-11 16:41:19 +0600

    project 4.6

--- app/src/main/java/com/example/shop/MainActivity.kt
+++ app/src/main/java/com/example/shop/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
+
+    //final int REQUEST_CODE_ADD_NOTE = 1;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button = findViewById(R.id.floatingActionButton)
 
     }
+
 }
